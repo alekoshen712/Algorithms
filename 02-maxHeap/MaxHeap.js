82a2686a4c0d764de55a5c16088d0e7c7e18234e
AlekoShen <alekoshen712@gmail.com>
2018-12-24 17:13:05 +0800

    maxHeap

---
+++ 02-maxHeap/MaxHeap.js
@@ -0,0 +1,70 @@
+import { swap } from "../until";
+
+class MaxHeap {
+  data = [];
+
+  constructor() {
+    if (Array.isArray(arr)) this.heapify(arr);
+  }
+
+  heapify(arr) {
+    if (arr.length) {
+      this.data.push(...arr);
+      for (let i = Math.floor((this.size() - 1 - 1) / 2); i >= 0; i--) {
+        this.shiftDown(i);
+      }
+    }
+  }
+
+  shiftUp(k) {
+    let data = this.data;
+    let parentIdx = Math.floor((k - 1) / 2);
+    while (k > 0 && data[parentIdx] < data[k]) {
+      swap(data, parentIdx, k);
+      k = parentIdx;
+      parentIdx = Math.floor((k - 1) / 2);
+    }
+  }
+
+  shiftDown(k) {
+    let [data, size] = [this.data, this.size()];
+    while (2 * k + 1 < size) {
+      let j = 2 * k + 1;
+      if (j + 1 < size && data[j + 1] > data[j]) j++;
+      if (data[k] >= data[j]) break;
+      swap(data, k, j);
+      k = j;
+    }
+  }
+
+  size() {
+    return this.data.length;
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  insert(num) {
+    this.data.push(num);
+    this.shiftUp(this.data.length);
+  }
+
+  extractMax() {
+    let data = this.data;
+    let size = this.size();
+    let ret = data[0];
+    if (size === 0) throw Error("The heap is empty!");
+    swap(data, 0, size - 1);
+    data.splice(size - 1, 1);
+    this.shiftDown(0);
+    return ret;
+  }
+}
+
+let m = new MaxHeap([3, 4, 6, 1, 9, 2, 8]);
+let arr = [];
+while (!m.isEmpty()) {
+  arr.unshift(m.extractMax());
+}
+console.log(arr);
